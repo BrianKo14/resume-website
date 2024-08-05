d18109675d88fd6794f34da47ade0fe7bca823a5
BrianKo14 <23661488+BrianKo14@users.noreply.github.com>
2024-08-05 12:36:31 -0300

    Projects: compilation

---
+++ projects/compile.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+# Hard coded values
+directory="./templates"
+blueprint="./blueprint.html"
+outputDirectory="./build"
+templateKey="<span id=\"template\"></span>"
+
+# Read the contents of blueprint
+blueprintContent=$(<"$blueprint")
+
+# Loop over all files in the given directory
+for template in "$directory"/*; do
+	if [ -f "$template" ]; then
+
+		# Read the contents of template
+		templateContent=$(<"$template")
+
+		# Replace "key" with the contents of template
+		result="${blueprintContent//$templateKey/$templateContent}"
+
+		# Determine the output file name
+		baseName=$(basename "$template")
+		outputFile="$outputDirectory/$baseName"
+
+		# Write the result to the output file
+		echo "$result" >"$outputFile"
+
+		echo "Output written to $outputFile"
+	fi
+done
---
+++ projects/readme.md
@@ -0,0 +1,4 @@
+
+## Projects showcase
+
+To simplify, I used only plain HTML/CSS/JS. I built a boilerplate for each project (`projects/blueprint.html`) that is compiled to each project's site (`projects/build/someproject.html`) by replacing the `<span id="template"></span>` with the contents of `projects/templates/someproject.html`. This is done by running `projects/compile.sh`.
